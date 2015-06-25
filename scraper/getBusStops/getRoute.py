c3476944f053fd26e09d9e1bdc7646cf9c9c908d
bobo <vikasyadavgood@gmail.com>
2015-06-25 16:38:43 +0530

    online search added

--- scraper/getBusStops/getRoute.py
+++ scraper/getBusStops/getRoute.py
@@ -6,8 +6,15 @@ how it works?
 Picks the buses from busroutes.csv 
 Gets their route from mybmtc in form of json
 
-Start from while loop to understand the flow complete 
+Start from while loop to understand the flow complete
+
+1) Searches if the bus is present in the local files
+2) If the bus is not found locally then looks up online 
+
+To do:
+Need to append the url of the bus found online locally 
 '''
+import signal, sys
 import requests, json, csv
 from BeautifulSoup import BeautifulSoup
 
@@ -40,15 +47,38 @@ def generateBusData(filename):
         #getRoutes(route_url)
     return buses
 
+def getNewBus(busno):
+	'''
+	Given any bus Number, this prints its route in one direction 
+	You can print routes in both directions by doing a small mod 
+	'''
+	bus_url = "http://www.mybmtc.com/route/search/"+busno+"/0"
+	soup = BeautifulSoup(requests.get(bus_url).text)
+	m = soup.find('a', {'id':'mapview'})
+	#use findAll for both directions and then loop to print
+	try: 
+		getRoutes(m['href'])
+	except:
+		print "Bus not found online also, sorry"
+	
+
 buses = generateBusData('busroutes.txt')
+
+
 
 while 1:
     #Given a bus number this returns the json file containing bus route
     try:
-        getRoutes(buses[raw_input("Enter Bus No Here: ").lower()])
+    	userInput = raw_input("Enter Bus No Here: ").lower()
+        getRoutes(buses[userInput])
         print "\n\n"
-    except:
-        print "Bus not found"
-    '''
-    You can make this call for all the buses in busroutes.csv file and save it a DB 
-    '''
+    except KeyboardInterrupt:
+		print "Exiting"
+		sys.exit()
+    except :
+    	print "Searching Online for Bus records"
+		getNewBus(userInput)
+		
+'''
+You can make this call for all the buses in busroutes.csv file and save it a DB 
+'''
